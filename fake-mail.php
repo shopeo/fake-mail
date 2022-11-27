b0c40f5b4a0f1da8231553895de15c2ecd5ad1b7
jabber <2898117012@qq.com>
2022-11-28 02:28:52 +0800

    init

---
+++ fake-mail.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Fake Mail
+ * Plugin URI: https://wordpress.org/plugins/fake-mail
+ * Description: WordPress Mail Log and disable!
+ * Author: Shopeo
+ * Version: 0.0.1
+ * Author URI: https://shopeo.cn
+ * License: GPL2+
+ * Text Domain: fake-mail
+ * Domain Path: /languages
+ * Requires at least: 5.9
+ * Requires PHP: 5.6
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
